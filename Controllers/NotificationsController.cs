baccfca338ace976954fbdd3da4a47efb6dd2114
Kamal Poladov <poladov16@mail.ru>
2021-02-13 17:25:54 +0400

    emojies

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using EmojiInTextarea.Models.ViewModels.Notifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmojiInTextarea.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        [HttpGet]
+        public IActionResult getNotification()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult getNotification(NotificationsViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/Notifications/NotificationsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmojiInTextarea.Models.ViewModels.Notifications
+{
+    public class NotificationsViewModel
+    {
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Message { get; set; }
+    }
+}
